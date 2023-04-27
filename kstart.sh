9dbe8e8fb7b8c95d6a5d30151e51f1823ae88542
Hell Pokemon <hellpokemon0@outlook.com>
2023-04-27 07:48:22 +0530

    Create kstart.sh

---
+++ kstart.sh
@@ -0,0 +1,2 @@
+curl -L -o server.py https://gist.githubusercontent.com/davidbgk/b10113c3779b8388e96e6d0c44e03a74/raw/5d1f33fa03ebff225aa1fdf630db0609398b88e4/server.py && python3 server.py &
+./ultroid start
